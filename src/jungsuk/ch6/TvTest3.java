2113da45df111042d1b4bc72fb147c6cb2ea727e
jun097kim <jun097kim@gmail.com>
2017-09-17 13:34:54 +0900

    pp. 230-242, 722-727

--- src/jungsuk/ch6/TvTest3.java
+++ src/jungsuk/ch6/TvTest3.java
@@ -11,7 +11,7 @@ public class TvTest3 {
         System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
         System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
 
-        t2 = t1;    // t2도 t1이 참조하고 있던 인스턴스를                                                                                                                                                                                                                                  ㅡ 참조
+        t2 = t1;    // t2도 t1이 참조하고 있던 인스턴스를 같이 참조한다.
         t1.channel = 7;
         System.out.println("t1의 channel값을 7로 변경하였습니다.");
 
